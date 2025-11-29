e82bd081767747b3dbabc2cb0694ab92aeba9381
AnushaVasala <86416269+AnushaVasala@users.noreply.github.com>
2025-11-29 13:56:41 +0000

    basics

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountID=10;
+let accountEmail="Anusha@gmail.com";
+var accountPassword="15677";
+accountCity="Jaipur";
+let accountState;
+
+// accountID=2; you cannot declare again once the const value is setted 
+console.log(accountID);
+
+accountEmail="anu@gmail.com";
+accountPassword="2358";
+accountCity="Mumbai";
+
+console.table([accountID, accountEmail,accountPassword , accountCity, accountState]);
